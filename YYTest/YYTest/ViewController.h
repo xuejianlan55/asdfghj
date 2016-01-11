e1b722853b140f2d5b062ff0c03496975cc88f81
朱明钧 <MZJ@feng13.local>
2016-01-12 00:33:52 +0800

    dfajflakjfafa

---
+++ YYTest/YYTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YYTest
+//
+//  Created by 朱明钧 on 16/1/12.
+//  Copyright © 2016年 朱明钧. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YYTest/YYTestTests/YYTestTests.m
@@ -0,0 +1,39 @@
+//
+//  YYTestTests.m
+//  YYTestTests
+//
+//  Created by 朱明钧 on 16/1/12.
+//  Copyright © 2016年 朱明钧. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface YYTestTests : XCTestCase
+
+@end
+
+@implementation YYTestTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
